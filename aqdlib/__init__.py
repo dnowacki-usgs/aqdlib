0c22955d51b8ba7fa07e245c941c9ed8ecfa6873
Dan Nowacki <dnowacki@usgs.gov>
2017-07-24 14:50:04 -0400

    Import updates

--- aqdlib/__init__.py
+++ aqdlib/__init__.py
@@ -1,3 +1,4 @@
 from .aqdhdr2cdf import load
+from .aqdcdf2nc import cdf_to_nc
 from .globalatts import read_globalatts
-from .qaqc import plot_inwater, atmcomp, plot_atmcomp
+from .qaqc import plot_inwater, atmcomp, plot_atmcomp, coord_transform
