f59a224d94f4a7a3d41d3b81ecf128bcc46269d6
Dan Nowacki <dnowacki@usgs.gov>
2017-07-25 17:37:45 -0400

    Tons of code improvements and expansion

--- aqdlib/aqdhdr2cdf.py
+++ aqdlib/aqdhdr2cdf.py
@@ -16,47 +16,35 @@ def load(basefile, metadata):
     # get instrument metadata from the HDR file
     instmeta = read_aqd_hdr(basefile)
 
-    # update metadata from Aquadopp header to CMG standard so that various
-    # profilers have the same attribute wording.  Redundant, but necessary
-    metadata['bin_count'] = instmeta['AQDNumberOfCells']
-    metadata['bin_size'] = instmeta['AQDCellSize']/100 # from cm to m
-    metadata['blanking_distance'] = instmeta['AQDBlankingDistance'] # already in m
-    # Nortek lists the distance to the center of the first bin as the blanking
-    # distance plus one cell size
-    metadata['center_first_bin'] = metadata['blanking_distance'] + metadata['bin_size'] # in m
-
     print "Loading ASCII files"
 
     # basePath = os.path.dirname(basefile)
     # baseName = os.path.basename(basefile)
     RAW = {}
+
     # Load sensor data
-    RAW = load_sen(RAW, basefile)
+    RAW = load_sen(RAW, basefile, metadata)
+
     # Load amplitude and velocity data
     RAW = load_amp_vel(RAW, basefile)
 
+    # Compute time stamps
     RAW = compute_time(RAW)
 
-    print metadata['orientation']
-    print 'Center_first_bin = %f\n' % metadata['center_first_bin']
-    print 'bin_size = %f\n' % metadata['bin_size']
-    print 'bin_count = %f\n' % metadata['bin_count']
-    RAW['bindist'] = np.arange(metadata['center_first_bin'], (metadata['center_first_bin']+((metadata['bin_count']-1)*metadata['bin_size'])), metadata['bin_size'])
+    # Deal with metadata peculiarities
+    metadata = check_metadata(metadata, instmeta)
 
-    if metadata['orientation'] == 'UP':
-        print 'User instructed that instrument was pointing UP'
-        # depth, or distance below surface, is a positive number below the
-        # surface, negative above the surface, for CMG purposes and consistency with ADCP
-        RAW['Depths'] = (metadata['WATER_DEPTH'] - metadata['transducer_offset_from_bottom']) - RAW['bindist']
-        Depth_NOTE = 'user reports uplooking bin depths = water_depth - transducer offset from bottom - bindist'
-    elif metadata['orientation'] == 'DOWN':
-        print 'User instructed that instrument was pointing DOWN'
-        RAW['Depths'] = (metadata['WATER_DEPTH'] - metadata['transducer_offset_from_bottom']) + RAW['bindist']
-        Depth_NOTE = 'user reports downlooking bin depths = water_depth - transducer_offset_from_bottom + bindist'
+    RAW = check_orientation(RAW, metadata)
 
-    metadata['INST_TYPE'] = 'Nortek Aquadopp Profiler';
+    # TODO: clock drift code
+    # TODO: Move time to center of ensemble??
+    # TODO: logmeta code
 
     RAW['instmeta'] = instmeta
+
+    # Put in fill values
+    print "about to insert fill values"
+    RAW = insert_fill_values(RAW, doublefill)
 
     # configure file
     cdf_filename = '/Volumes/Backstaff/field/gb_proc/1076a/1076a1aqd/' + metadata['filename'] + '-raw.cdf' # TODO: fix the path
@@ -74,8 +62,81 @@ def load(basefile, metadata):
 
     return RAW
 
+def insert_fill_values(RAW, doublefill):
+    print "Inserting fill values"
+    for k in RAW:
+        if k not in ['instmeta', 'time', 'time2', 'datetime'] and np.max(np.shape(RAW[k])) == np.max(np.shape(RAW['jd'])):
+            nanind = np.where(np.isnan(RAW[k]))
+            RAW[k][nanind] = doublefill
+
+    return RAW
+
+def check_orientation(RAW, metadata):
+    print metadata['orientation']
+    print 'Center_first_bin = %f\n' % metadata['center_first_bin']
+    print 'bin_size = %f\n' % metadata['bin_size']
+    print 'bin_count = %f\n' % metadata['bin_count']
+    # TODO: these values are already in the HDR file...
+    RAW['bindist'] = np.arange(metadata['center_first_bin'], (metadata['center_first_bin']+((metadata['bin_count']-1)*metadata['bin_size'])), metadata['bin_size'])
+
+    if metadata['orientation'] == 'UP':
+        print 'User instructed that instrument was pointing UP'
+        # depth, or distance below surface, is a positive number below the
+        # surface, negative above the surface, for CMG purposes and consistency with ADCP
+        RAW['Depths'] = (metadata['WATER_DEPTH'] - metadata['transducer_offset_from_bottom']) - RAW['bindist']
+        Depth_NOTE = 'user reports uplooking bin depths = water_depth - transducer offset from bottom - bindist' # TODO: this is never used
+    elif metadata['orientation'] == 'DOWN':
+        print 'User instructed that instrument was pointing DOWN'
+        RAW['Depths'] = (metadata['WATER_DEPTH'] - metadata['transducer_offset_from_bottom']) + RAW['bindist']
+        Depth_NOTE = 'user reports downlooking bin depths = water_depth - transducer_offset_from_bottom + bindist' # TODO: this is never used
+
+    return RAW
+
+def check_metadata(metadata, instmeta):
+
+    # % now verify the global metadata for standard EPIC and cmg stuff
+    # % everything in metadata and instmeta get written as global attributes
+    # % these also get copied to the .nc file
+    if 'initial_instrument_height' not in metadata or np.isnan(metadata['initial_instrument_height']):
+        metadata['initial_instrument_height'] = 0
+
+    for k in instmeta:
+        print k
+    metadata['serial_number'] = instmeta['AQDSerial_Number']
+
+    # update metadata from Aquadopp header to CMG standard so that various
+    # profilers have the same attribute wording.  Redundant, but necessary
+    metadata['bin_count'] = instmeta['AQDNumberOfCells']
+    metadata['bin_size'] = instmeta['AQDCellSize'] / 100 # from cm to m
+    metadata['blanking_distance'] = instmeta['AQDBlankingDistance'] # already in m
+    # Nortek lists the distance to the center of the first bin as the blanking
+    # distance plus one cell size
+    metadata['center_first_bin'] = metadata['blanking_distance'] + metadata['bin_size'] # in m
+
+
+    metadata['salinity_set_by_user'] = instmeta['AQDSalinity']
+    metadata['salinity_set_by_user_units'] = 'ppt'
+
+    metadata['frequency'] = instmeta['AQDFrequency']
+    metadata['beam_width'] = instmeta['AQDBeamWidth']
+    metadata['beam_pattern'] = instmeta['AQDBeamPattern']
+    metadata['beam_angle'] = instmeta['AQDBeamAngle']
+    # instmeta['AQDHeadRotation'] = metadata.pop('head_rotation') # also deletes this key. Not sure why the Matlab file does this, maybe need TODO look into this
+
+    # TODO: figure these out
+    # metadata['insterr'] = instmeta['error']
+    # metadata['inststat'] = instmeta['status']
+    # metadata['instorient'] = instmeta['orient']
+
+    metadata['INST_TYPE'] = 'Nortek Aquadopp Profiler';
+
+    return metadata
 
 def add_min_max(cdf_filename):
+    """
+    Add minimum and maximum values to variables in CDF file
+    """
+
     rg = Dataset(cdf_filename, 'r+')
     exclude = rg.dimensions.keys()
     exclude.extend(('time2', 'TransMatrix'))
@@ -117,6 +178,12 @@ def write_aqd_cdf_data(cdf_filename, RAW, metadata):
     rg['Heading'][:] = RAW['heading']
 
     rg['TransMatrix'][:] = RAW['instmeta']['AQDTransMatrix']
+
+    if 'AnaInp1' in RAW:
+        rg['AnalogInput1'][:] = RAW['AnaInp1']
+
+    if 'AnaInp2' in RAW:
+        rg['AnalogInput2'][:] = RAW['AnaInp2']
 
     rg.close()
 
@@ -262,11 +329,38 @@ def define_aqd_cdf_file(cdf_filename, RAW, metadata):
     Tmatid = rg.createVariable('TransMatrix', 'f', ('Tmatrix', 'Tmatrix',))
     Tmatid.long_name = 'Transformation Matrix for this Aquadopp'
 
-    rg.close()
+    for n in ['1', '2']:
+        if 'AnalogInput' + n in metadata:
+            Anaid = rg.createVariable('AnalogInput' + n, 'f', ('time',))
+            Anaid.units = 'Volts'
+            Anaid.sensor_type = metadata['AnalogInput1']['sensor_type']
+            Anaid.sensor_manufacturer = metadata['AnalogInput1']['sensor_manufacturer']
+            Anaid.sensor_model = metadata['AnalogInput1']['sensor_model']
+            Anaid.serial_number = metadata['AnalogInput1']['serial_number']
 
-    # except:
-    #     print 'exception raised'
-    #     rg.close()
+            if 'initial_sensor_height' in metadata['AnalogInput' + n]:
+                # TODO
+                # metadata.AnalogInput1.nominal_sensor_depth = metadata.WATER_DEPTH - metadata.AnalogInput1.initial_sensor_height;
+                # netcdf.putAtt(ncid,Ana1id,'initial_sensor_height',metadata.AnalogInput1.initial_sensor_height);
+                # netcdf.putAtt(ncid,Ana1id,'nominal_sensor_depth',metadata.AnalogInput1.nominal_sensor_depth);
+                continue
+            elif 'nominal_sensor_depth' in metadata['AnalogInput' + n]: # TODO: should be another if not elif??
+                # netcdf.putAtt(ncid,Ana1id,'nominal_sensor_depth',metadata.AnalogInput1.nominal_sensor_depth);
+                # metadata.AnalogInput1.initial_sensor_height = metadata.WATER_DEPTH - metadata.AnalogInput1.nominal_sensor_depth;
+                # netcdf.putAtt(ncid,Ana1id,'initial_sensor_height',metadata.AnalogInput1.initial_sensor_height);
+                continue
+
+        # if isfield(metadata.AnalogInput1,'range'),
+        #         netcdf.putAtt(ncid,Ana1id,'range',metadata.AnalogInput1.range);
+        # end
+        # if isfield(metadata.AnalogInput1.cals,'NTUcoef'),
+        #         netcdf.putAtt(ncid,Ana1id,'NTUcoef',metadata.AnalogInput1.cals.NTUcoef);
+        # end
+        # if isfield(metadata.AnalogInput1.cals,'SEDcoef'),
+        #         netcdf.putAtt(ncid,Ana1id,'SEDcoef',metadata.AnalogInput1.cals.SEDcoef);
+        # end
+
+    rg.close()
 
 def write_metadata(rg, metadata):
     """
@@ -301,9 +395,12 @@ def compute_time(RAW):
     # TODO: Hopefully this is correct... roundoff errors on big numbers...
     RAW['time2'] = np.round((RAW['jd'] - RAW['time'])*86400000)
 
+    # TODO: start_time and stop_time into metadata
+    # metadata['start_time'] =
+
     return RAW
 
-def load_sen(RAW, basefile):
+def load_sen(RAW, basefile, metadata):
     """
     Load data from .sen file
     """
@@ -320,6 +417,13 @@ def load_sen(RAW, basefile):
     for year, month, day, hour, minute, second in zip(SEN[:,2], SEN[:,0], SEN[:,1], SEN[:,3], SEN[:,4], SEN[:,5]):
         RAW['datetime'].append(dt.datetime(int(year), int(month), int(day), int(hour), int(minute), int(second), tzinfo=pytz.utc))
     RAW['datetime'] = np.array(RAW['datetime'])
+
+    # Look for analog data
+    if 'AnalogInput1' in metadata:
+        RAW['AnaInp1'] = SEN[:,15] * 5 / 65535
+
+    if 'AnalogInput2' in metadata:
+        RAW['AnaInp2'] = SEN[:,16] * 5 / 65535
 
     return RAW
 
@@ -408,11 +512,45 @@ def read_aqd_hdr(basefile):
         elif 'Comments' in row:
             Instmeta['AQDComments'] = row[38:]
 
-    while 'Data file format' not in row:
+    while 'Head configuration' not in row:
         row = f.readline().rstrip()
-        if 'Head frequency' in row:
+        if 'Serial number' in row:
+            Instmeta['AQDSerial_Number'] = row[38:]
+        elif 'Hardware revision' in row:
+            Instmeta['AQDHardwareRevision'] = row[38:]
+        elif 'Revision number' in row:
+            Instmeta['AQDRevisionNumber'] = row[38:]
+        elif 'Recorder size' in row:
+            Instmeta['AQDRecorderSize'] = row[38:]
+        elif 'Firmware version' in row:
+            Instmeta['AQDFirmwareVersion'] = row[38:]
+        elif 'Velocity range' in row:
+            Instmeta['AQDVelocityRange'] = row[38:]
+        elif 'Power output' in row:
+            Instmeta['AQDAnalogPowerOutput'] = row[38:]
+        elif 'Analog input #1 calibration (a0, a1)' in row:
+            Instmeta['AQDAnalogInputCal1'] = float(row[38:])
+        elif 'Analog input #2 calibration (a0, a1)' in row:
+            Instmeta['AQDAnalogInputCal2'] = float(row[38:])
+        elif 'Sync signal data out delay' in row:
+            Instmeta['AQDSyncOutDelay'] = row[38:]
+        elif 'Sync signal power down delay' in row:
+            Instmeta['AQDSyncPowerDelay'] = row[38:]
+
+    while 'Current profile cell center distance from head (m)' not in row:
+    # while 'Data file format' not in row:
+        row = f.readline().rstrip()
+        if 'Pressure sensor' in row:
+            Instmeta['AQDPressureSensor'] = row[38:]
+        elif 'Compass' in row:
+            Instmeta['AQDCompass'] = row[38:]
+        elif 'Tilt sensor' in row:
+            Instmeta['AQDTilt'] = row[38:]
+        elif 'Head frequency' in row:
             idx = row.find(' kHz')
             Instmeta['AQDFrequency'] = float(row[38:idx])
+        elif 'Number of beams' in row:
+            Instmeta['AQDNumBeams'] = float(row[38:])
         elif 'Serial number' in row:
             Instmeta['AQDHeadSerialNumber'] = row[38:]
         elif 'Transformation matrix' in row:
@@ -422,8 +560,6 @@ def read_aqd_hdr(basefile):
                 row = f.readline().rstrip()
         elif 'Pressure sensor calibration' in row:
             Instmeta['AQDPressureCal'] = row[38:]
-
-    return Instmeta
 
     #          elseif (strfind(str,'Wave measurements'))
     #              Instmeta.WaveMeasurements = (str(39:end));
@@ -441,57 +577,21 @@ def read_aqd_hdr(basefile):
     #                  Instmeta.WaveCellSize = str(39:42);
     #             end
 
-# % getting the hardware configuration
-# while isempty(strfind(str,'Head configuration'));
-#      str=fgetl(hdr);
-#          if (strfind(str,'Compass'))
-#              Instmeta.AQDCompass = str(39:end);
-#          elseif (strfind(str,'Tilt sensor'))
-#              Instmeta.AQDTilt = str(39:end);
-#          elseif (strfind(str,'Pressure sensor calibration'))
-#              Instmeta.AQDPressureCal = str2num(str(39:end));
-#          elseif (strfind(str,'Number of beams'))
-#              Instmeta.AQDNumBeams = str2num(str(39:end));
-#          elseif (strfind(str,'Serial number'))
-#              %Instmeta.SERIAL_NUMBER = str(39:end);
-#              Instmeta.AQDSerial_Number = str(39:end);
-#          elseif (strfind(str,'Internal code version'))
-#              Instmeta.AQDInternalCodeVersion = str(39:end);
-#          elseif (strfind(str,'Revision number'))
-#              Instmeta.AQDRevisionNumber = str(39:end);
-#          elseif (strfind(str,'Recorder size'))
-#              Instmeta.AQDRecorderSize = str(39:end);
-#          elseif (strfind(str,'Firmware version'))
-#              Instmeta.AQDFirmwareVersion = str(39:end);
-#          elseif (strfind(str,'Power output'))
-#              Instmeta.AQDAnalogPowerOutput = str(39:end);
-#          elseif (strfind(str,'Analog input #1 calibration (a0, a1)'))
-#              Instmeta.AQDAnalogInputCal1 = str2num(str(39:end));
-#          elseif (strfind(str,'Analog input #2 calibration (a0, a1)'))
-#              Instmeta.AQDAnalogInputCal2 = str2num(str(39:end));
-#          elseif (strfind(str,'Sync signal data out delay'))
-#              Instmeta.AQDSyncOutDelay = str(39:end);
-#          elseif (strfind(str,'Sync signal power down delay'))
-#              Instmeta.AQDSyncPowerDelay = str(39:end);
-#          end
-# end
 
-#
-# % infer some things based on the Aquadopp brochure
-# switch Instmeta.AQDFrequency
-#     case 400
-#         Instmeta.AQDBeamWidth = 3.7;
-#     case 600
-#         Instmeta.AQDBeamWidth = 3.0;
-#     case 1000
-#         Instmeta.AQDBeamWidth = 3.4;
-#     case 2000
-#         Instmeta.AQDBeamWidth = 1.7;
-#     otherwise
-#         Instmeta.AQDBeamWidth = NaN;
-# end
-# Instmeta.AQDBeamPattern = 'convex';
-# Instmeta.AQDBeamAngle = 25;
+    # % infer some things based on the Aquadopp brochure
+    if Instmeta['AQDFrequency'] == 400:
+        Instmeta['AQDBeamWidth'] = 3.7
+    elif Instmeta['AQDFrequency'] == 600:
+        Instmeta['AQDBeamWidth'] = 3.0
+    elif Instmeta['AQDFrequency'] == 1000:
+        Instmeta['AQDBeamWidth'] = 3.4
+    elif Instmeta['AQDFrequency'] == 2000:
+        Instmeta['AQDBeamWidth'] = 1.7
+    else:
+        Instmeta['AQDBeamWidth'] = np.nan
+
+    Instmeta['AQDBeamPattern'] = 'convex'
+    Instmeta['AQDBeamAngle'] = 25;
 # Instmeta.AQDVelRange = 1000; % cm/s
 # Instmeta.AQDTempRange = [-4 40];
 # Instmeta.AQDPressRange = [0 100];
@@ -505,3 +605,5 @@ def read_aqd_hdr(basefile):
 # %     Instmeta = rmfield(Instmeta,fields);
 # % else
 # % end
+
+    return Instmeta
