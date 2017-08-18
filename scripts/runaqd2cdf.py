6b37db569bdff4041dbae66d1abdd005f9a0c751
Dan Nowacki <dnowacki@usgs.gov>
2017-08-18 15:46:35 -0400

    Specify basename in the yaml file instead

--- scripts/runaqd2cdf.py
+++ scripts/runaqd2cdf.py
@@ -7,7 +7,7 @@ import argparse
 import yaml
 
 parser = argparse.ArgumentParser(description='Convert Aquadopp text files to raw .cdf format. Run this script from the directory containing Aquadopp files')
-parser.add_argument('basename', help='base name (without extension) of the Aquadopp text files')
+# parser.add_argument('basename', help='base name (without extension) of the Aquadopp text files')
 parser.add_argument('gatts', help='path to global attributes file (gatts formatted)')
 parser.add_argument('config', help='path to ancillary config file (YAML formatted)')
 # parser.add_argument('-v', '--verbose', help='increase output verbosity', action='store_true')
@@ -23,4 +23,4 @@ config = yaml.safe_load(open(args.config))
 for k in config:
     metadata[k] = config[k]
 
-RAW = aqdlib.prf_to_cdf(args.basename, metadata)
+RAW = aqdlib.prf_to_cdf(metadata['basefile'], metadata)
