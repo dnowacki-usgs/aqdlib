1fdabadecc7ea0182088f2c9980f4b7bdcc00eba
Dan Nowacki <dnowacki@usgs.gov>
2017-09-14 16:57:52 -0400

    Deleting old code, reordering code, unnecessary imports

--- aqdlib/aqdcdf2nc.py
+++ aqdlib/aqdcdf2nc.py
@@ -1,26 +1,39 @@
 from __future__ import division, print_function
-from netCDF4 import Dataset
-import numpy as np
-import qaqc
-import dateutil
-import pytz
-import jdcal
-import datetime as dt
-import aqdlib
 import xarray as xr
 import pandas as pd
+import numpy as np
+import qaqc
+import aqdlib
 
 def cdf_to_nc(cdf_filename, metadata, p_1ac=False):
 
-    nc_filename = metadata['filename'] + '.nc'
+    VEL = load_cdf(cdf_filename, metadata, p_1ac=p_1ac)
 
-    VEL = {}
+    # Create INFO dict with global attributes from dataset
+    INFO = VEL.attrs
 
-    VEL, INFO = load_cdf_amp_vel(cdf_filename, VEL, metadata, p_1ac=p_1ac)
+    # Clip data to in/out water times or via good_ens
+    VEL = clip_ds(VEL, metadata)
 
-    # define_aqd_nc_file(nc_filename, VEL, metadata, INFO)
+    # Create water_depth variables
+    VEL, metadata = qaqc.create_water_depth(VEL, metadata)
 
-    # write_aqd_nc_file(nc_filename, VEL, metadata)
+    VEL, T = qaqc.set_orientation(VEL, VEL['TransMatrix'].values, metadata, INFO)
+
+    u, v, w = qaqc.coord_transform(VEL['VEL1'].values, VEL['VEL2'].values, VEL['VEL3'].values,
+        VEL['Heading'].values, VEL['Pitch'].values, VEL['Roll'].values, T, VEL.attrs['AQDCoordinateSystem'])
+
+    VEL['U'] = xr.DataArray(u, dims=('time', 'bindist'))
+    VEL['V'] = xr.DataArray(v, dims=('time', 'bindist'))
+    VEL['W'] = xr.DataArray(w, dims=('time', 'bindist'))
+
+    VEL = qaqc.magvar_correct(VEL, metadata)
+
+    VEL['AGC'] = (VEL['AMP1'] + VEL['AMP2'] + VEL['AMP3']) / 3
+
+    VEL = qaqc.trim_vel(VEL, metadata, INFO)
+
+    VEL = qaqc.make_bin_depth(VEL, metadata)
 
     # TODO: Need to add all global attributes from CDF to NC file (or similar)
     qaqc.add_min_max(VEL)
@@ -29,12 +42,13 @@ def cdf_to_nc(cdf_filename, metadata, p_1ac=False):
     qaqc.add_final_metadata(VEL)
     print('Added final metadata')
 
-    # VEL = VEL.expand_dims('lat')
-    # need to associate dimensions with lat/lon
+    # Reshape and associate dimensions with lat/lon
     for var in ['U', 'V', 'W', 'AGC', 'Pressure', 'Temperature', 'Heading', 'Pitch', 'Roll']:
         VEL[var] = reshape(VEL[var])
 
     VEL = rename(VEL, metadata, INFO)
+
+    nc_filename = metadata['filename'] + '.nc'
 
     VEL.to_netcdf(nc_filename)
     print('Done writing NetCDF file', nc_filename)
@@ -60,12 +74,10 @@ def rename(ds, metadata, INFO):
     Rename DataArrays, drop unused DataArrays, and add attrs
     """
 
-    # put in attributes common to all velocity components
     def add_vel_attributes(vel, metadata, INFO):
         vel.attrs.update({'units': 'cm/s',
             'data_cmnt': 'Velocity in shallowest bin is often suspect and should be used with caution'})
 
-        add_attributes(vel, metadata, INFO)
         # TODO: why do we only do trim_method for Water Level SL?
         if 'trim_method' in metadata and metadata['trim_method'].lower() == 'water level sl':
             vel.attrs.update({'note': 'Velocity bins trimmed if out of water or if side lobes intersect sea surface'})
@@ -74,8 +86,10 @@ def rename(ds, metadata, INFO):
         var.attrs.update({'serial_number': INFO['AQDSerial_Number'],
             'initial_instrument_height': metadata['initial_instrument_height'],
             'nominal_instrument_depth': metadata['nominal_instrument_depth'],
-            'height_depth_units': 'm', 'sensor_type': INFO['INST_TYPE']})
+            'height_depth_units': 'm', 'sensor_type': INFO['INST_TYPE'],
+            '_FillValue': 1e35})
 
+    # Rename DataArrays for EPIC compliance
     varnames = {'U': 'u_1205',
             'V': 'v_1206',
             'W': 'w_1204',
@@ -88,41 +102,83 @@ def rename(ds, metadata, INFO):
 
     ds.rename(varnames, inplace=True)
 
-    todrop = ['VEL1', 'VEL2', 'VEL3', 'AMP1', 'AMP2', 'AMP3', 'Battery', 'TransMatrix', 'AnalogInput1', 'AnalogInput2']
+    # Drop old DataArrays
+    todrop = ['VEL1', 'VEL2', 'VEL3',
+              'AMP1', 'AMP2', 'AMP3',
+              'Battery', 'TransMatrix', 'AnalogInput1', 'AnalogInput2']
     ds = ds.drop(todrop)
 
-    ds['u_1205'].attrs.update({'name': 'u', 'long_name': 'Eastward Velocity', 'generic_name': 'u', 'epic_code': 1205})
-    ds['v_1206'].attrs.update({'name': 'v', 'long_name': 'Northward Velocity', 'generic_name': 'v', 'epic_code': 1206})
-    ds['w_1204'].attrs.update({'name': 'w', 'long_name': 'Vertical Velocity', 'generic_name': 'w', 'epic_code': 1204})
-    ds['P_1'].attrs.update({'units': 'dbar', 'name': 'P', 'long_name': 'Pressure', 'generic_name': 'depth', 'epic_code': 1}) # TODO: is this generic name correct?
-    ds['Tx_1211'].attrs.update({'units': 'C', 'name': 'Tx', 'long_name': 'Instrument Transducer Temperature', 'generic_name': 'temp', 'epic_code': 1211})
-    ds['AGC_1202'].attrs.update({'units': 'counts', 'name': 'AGC', 'long_name': 'Average Echo Intensity', 'generic_name': 'AGC', 'epic_code': 1202})
-    # AGCid.sensor_type = INFO['INST_TYPE']
+    # Update attributes for EPIC and STG compliance
+    ds['u_1205'].attrs.update({'name': 'u',
+        'long_name': 'Eastward Velocity',
+        'generic_name': 'u',
+        'epic_code': 1205})
 
-    ds['Hdg_1215'].attrs.update({'units': 'degrees', 'name': 'Hdg', 'long_name': 'Instrument Heading', 'generic_name': 'hdg', 'epic_code': 1215})
+    ds['v_1206'].attrs.update({'name': 'v',
+        'long_name': 'Northward Velocity',
+        'generic_name': 'v',
+        'epic_code': 1206})
+
+    ds['w_1204'].attrs.update({'name': 'w',
+        'long_name': 'Vertical Velocity',
+        'generic_name': 'w',
+        'epic_code': 1204})
+
+    ds['P_1'].attrs.update({'units': 'dbar',
+        'name': 'P',
+        'long_name': 'Pressure',
+        'generic_name': 'depth',
+        'epic_code': 1}) # TODO: is this generic name correct?
+
+    ds['Tx_1211'].attrs.update({'units': 'C',
+        'name': 'Tx',
+        'long_name': 'Instrument Transducer Temperature',
+        'generic_name': 'temp',
+        'epic_code': 1211})
+
+    ds['AGC_1202'].attrs.update({'units': 'counts',
+        'name': 'AGC',
+        'long_name': 'Average Echo Intensity',
+        'generic_name': 'AGC',
+        'epic_code': 1202})
+
+    ds['Hdg_1215'].attrs.update({'units': 'degrees',
+        'name': 'Hdg',
+        'long_name': 'Instrument Heading',
+        'generic_name': 'hdg',
+        'epic_code': 1215})
+
     if 'magnetic_variation_at_site' in metadata:
         ds['Hdg_1215'].attrs.update({'note': 'Heading is degrees true. Converted from magnetic with magnetic variation of ' + str(metadata['magnetic_variation_at_site'])})
     elif 'magnetic_variation' in metadata:
         ds['Hdg_1215'].attrs.update({'note': 'Heading is degrees true. Converted from magnetic with magnetic variation of ' + str(metadata['magnetic_variation'])})
 
-    ds['Ptch_1216'].attrs.update({'units': 'degrees', 'name': 'Ptch', 'long_name': 'Instrument Pitch', 'generic_name': 'ptch', 'epic_code': 1216})
-    ds['Roll_1217'].attrs.update({'units': 'degrees', 'name': 'Roll', 'long_name': 'Instrument Roll', 'generic_name': 'roll', 'epic_code': 1217})
+    ds['Ptch_1216'].attrs.update({'units': 'degrees',
+        'name': 'Ptch',
+        'long_name': 'Instrument Pitch',
+        'generic_name': 'ptch',
+        'epic_code': 1216})
+    ds['Roll_1217'].attrs.update({'units': 'degrees',
+        'name': 'Roll',
+        'long_name': 'Instrument Roll',
+        'generic_name': 'roll',
+        'epic_code': 1217})
 
-    for v in ['P_1', 'Tx_1211', 'AGC_1202', 'Hdg_1215', 'Ptch_1216', 'Roll_1217']:
+    for v in ['P_1', 'Tx_1211', 'AGC_1202', 'Hdg_1215', 'Ptch_1216', 'Roll_1217', 'u_1205', 'v_1206', 'w_1204']:
         add_attributes(ds[v], metadata, INFO)
 
     for v in ['u_1205', 'v_1206', 'w_1204']:
         add_vel_attributes(ds[v], metadata, INFO)
 
-
     return ds
 
-def load_cdf_amp_vel(cdf_filename, VEL, metadata, p_1ac=False):
+def load_cdf(cdf_filename, metadata, p_1ac=False):
 
     ds = xr.open_dataset(cdf_filename, autoclose=True)
 
-    # Create INFO dict with global attributes from dataset
-    INFO = ds.attrs
+    return ds
+
+def clip_ds(ds, metadata):
 
     # clip either by ensemble indices or by the deployment and recovery date specified in metadata
     if 'good_ens' in metadata:
@@ -151,36 +207,11 @@ def load_cdf_amp_vel(cdf_filename, VEL, metadata, p_1ac=False):
         print('first burst in trimmed file:', ds['time'].min().values)
         print('last burst in trimmed file:', ds['time'].max().values)
 
-    print(ds.info)
-
-    VEL, metadata = qaqc.create_water_depth(ds, metadata)
-
-    T = ds['TransMatrix'].values
-
-    VEL, T = qaqc.set_orientation(ds, T, metadata, INFO)
-
-    u, v, w = qaqc.coord_transform(VEL['VEL1'].values, VEL['VEL2'].values, VEL['VEL3'].values,
-        VEL['Heading'].values, VEL['Pitch'].values, VEL['Roll'].values, T, VEL.attrs['AQDCoordinateSystem'])
-
-    VEL['U'] = xr.DataArray(u, dims=('time', 'bindist'))
-    VEL['V'] = xr.DataArray(v, dims=('time', 'bindist'))
-    VEL['W'] = xr.DataArray(w, dims=('time', 'bindist'))
-
-    VEL = qaqc.magvar_correct(VEL, metadata)
-
-    VEL['AGC'] = (VEL['AMP1'] + VEL['AMP2'] + VEL['AMP3']) / 3
-
-    VEL = qaqc.trim_vel(VEL, metadata, INFO)
-
-    VEL = qaqc.make_bin_depth(VEL, metadata)
-
-
-    return VEL, INFO
+    return ds
 
     #     if p_1ac is not False:
     #         VEL['press_ac'] = p_1ac[S:E]
     #
-    return VEL, INFO
 
 def define_aqd_nc_file(nc_filename, VEL, metadata, INFO):
 
@@ -225,22 +256,6 @@ def define_aqd_nc_file(nc_filename, VEL, metadata, INFO):
         bindistid.initial_instrument_height = metadata['initial_instrument_height']
         bindistid.note = 'distance is along profile from instrument head to center of bin'
 
-
-
-
-        add_vel_attributes(u_1205, metadata, INFO)
-
-        add_vel_attributes(v_1206, metadata, INFO)
-
-
-        add_vel_attributes(w_1204, metadata, INFO)
-
-
-        add_attributes(Pressid, metadata, INFO)
-        # TODO: why no sensor_type in these vars?
-
-
-
         # TODO: add analog input variables (OBS, NTU, etc)
 
         bdepid = rg.createVariable('bin_depth', 'd', ('depth', 'lat', 'lon', 'time',), zlib=True, fill_value=aqdlib.DOUBLE_FILL)
@@ -260,33 +275,3 @@ def define_aqd_nc_file(nc_filename, VEL, metadata, INFO):
             ACPressid.long_name = 'CORRECTED PRESSURE (DB)'
             add_attributes(ACPressid, metadata, INFO)
             ACPressid.note = 'Corrected for variations in atmospheric pressure using nearby MET station'
-
-def write_aqd_nc_file(nc_filename, VEL, metadata):
-
-    N, M = np.shape(VEL['U'])
-    print('N:', N, 'M:', M, 'in write_aqd_nc_file')
-
-    with Dataset(nc_filename, 'r+') as rg:
-
-        rg['lat'][:] = metadata['latitude']
-        rg['lon'][:] = metadata['longitude']
-        rg['time'][:] = VEL['time']
-        rg['time2'][:] = VEL['time2']
-        rg['depth'][:] = VEL['depths']
-
-        rg['bindist'][:] = VEL['bindist']
-        rg['bin_depth'][:] = np.reshape(VEL['bin_depth'].T, (M, 1, 1, N))
-        rg['u_1205'][:] = np.reshape(VEL['U'].T, (M, 1, 1, N))
-        rg['v_1206'][:] = np.reshape(VEL['V'].T, (M, 1, 1, N))
-        rg['w_1204'][:] = np.reshape(VEL['W'].T, (M, 1, 1, N))
-        rg['AGC_1202'][:] = np.reshape(VEL['AGC'].T, (M, 1, 1, N))
-
-        rg['Tx_1211'][:] = VEL['temp'][np.newaxis, np.newaxis, :]
-        rg['P_1'][:] = VEL['pressure'][np.newaxis, np.newaxis, :]
-
-        if 'press_ac' in VEL:
-            rg['P_1ac'][:] = VEL['press_ac'][np.newaxis, np.newaxis, :]
-
-        rg['Ptch_1216'][:] = VEL['pitch'][np.newaxis, np.newaxis, :]
-        rg['Roll_1217'][:] = VEL['roll'][np.newaxis, np.newaxis, :]
-        rg['Hdg_1215'][:] = VEL['heading'][np.newaxis, np.newaxis, :]
