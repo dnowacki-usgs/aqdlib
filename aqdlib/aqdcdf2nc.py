35468c9ae85f333d008ea6c971170bf2399e71dc
Dan Nowacki <dnowacki@usgs.gov>
2017-09-18 11:02:51 -0400

    Allow functions to run as scripts

--- aqdlib/aqdcdf2nc.py
+++ aqdlib/aqdcdf2nc.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from __future__ import division, print_function
 import xarray as xr
 import qaqc
@@ -288,3 +290,36 @@ def ds_add_attrs(ds, metadata):
         add_vel_attributes(ds[v], metadata)
 
     return ds
+
+def main():
+    import sys
+    sys.path.insert(0, '/Users/dnowacki/Documents/aqdlib')
+    import aqdlib
+    import argparse
+    import yaml
+
+    parser = argparse.ArgumentParser(description='Convert raw Aquadopp .cdf format to processed .nc files')
+    parser.add_argument('cdfname', help='raw .CDF filename')
+    parser.add_argument('gatts', help='path to global attributes file (gatts formatted)')
+    parser.add_argument('config', help='path to ancillary config file (YAML formatted)')
+    parser.add_argument('--atmpres', help='path to cdf file containing atmopsheric pressure data')
+
+    args = parser.parse_args()
+
+    # initialize metadata from the globalatts file
+    metadata = aqdlib.read_globalatts(args.gatts)
+
+    # Add additional metadata from metadata config file
+    config = yaml.safe_load(open(args.config))
+
+    for k in config:
+        metadata[k] = config[k]
+
+    if args.atmpres:
+        # press_ac = aqdlib.load_press_ac('press_ac.cdf', ['p_1ac'])
+        VEL = cdf_to_nc(args.cdfname, metadata, atmpres=args.atmpres)
+    else:
+        VEL = cdf_to_nc(args.cdfname, metadata)
+
+if __name__ == '__main__':
+    main()
