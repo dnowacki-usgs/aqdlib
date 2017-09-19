540735c74b97ad7d8bc87b99fae1a0c082a1c38a
Dan Nowacki <dnowacki@usgs.gov>
2017-09-19 16:00:02 -0400

    Save time as unlimited_dims

--- aqdlib/aqdcdf2nc.py
+++ aqdlib/aqdcdf2nc.py
@@ -57,7 +57,7 @@ def cdf_to_nc(cdf_filename, metadata, atmpres=False):
 
     nc_filename = metadata['filename'] + '.nc'
 
-    VEL.to_netcdf(nc_filename)
+    VEL.to_netcdf(nc_filename, unlimited_dims='time')
     print('Done writing netCDF file', nc_filename)
 
     return VEL
